6d3700e72fa70cb08319162117f422d4b5761245
AiMuranaka <ai.muranaka@carrot.co.jp>
2022-08-10 16:08:00 +0900

    Correction of the main function

--- Qt/CharacterMap-2/main.cpp
+++ Qt/CharacterMap-2/main.cpp
@@ -1,11 +1,11 @@
-#include "characterwidget.h"
+#include "mainwindow.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    CharacterWidget w;
+    MainWindow w;
     w.show();
     return a.exec();
 }
