cbef405a2aeac937838d5ef639042a0e092ffdc7
AiMuranaka <ai.muranaka@carrot.co.jp>
2022-11-02 14:01:08 +0900

    implement Wiggly

---
+++ Qt/Wiggly/wigglywidget.h
@@ -0,0 +1,26 @@
+#ifndef WIGGLYWIDGET_H
+#define WIGGLYWIDGET_H
+#include <QBasicTimer>
+#include <QWidget>
+
+class WigglyWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    WigglyWidget(QWidget *parent = nullptr);
+
+public slots:
+    void setText(const QString &newText) { text = newText; }
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+    void timerEvent(QTimerEvent *event) override;
+
+private:
+    QBasicTimer timer;
+    QString text;
+    int step;
+};
+
+#endif // WIGGLYWIDGET_H
