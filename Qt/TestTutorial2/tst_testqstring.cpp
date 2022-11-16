d653d4d120333129fba49648b39ae313d68ae57b
AiMuranaka <ai.muranaka@carrot.co.jp>
2022-11-16 09:27:34 +0900

    implement TestTutorial2

---
+++ Qt/TestTutorial2/tst_testqstring.cpp
@@ -0,0 +1,34 @@
+#include <QtTest>
+
+// add necessary includes here
+
+class TestQString : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void toUpper_data();
+    void toUpper();
+};
+
+void TestQString::toUpper_data()
+{
+    QTest::addColumn<QString>("string");
+    QTest::addColumn<QString>("result");
+
+    QTest::newRow("all lower") << "hello" << "HELLO";
+    QTest::newRow("mixed")     << "Hello" << "HELLO";
+    QTest::newRow("all upper") << "HELLO" << "HELLO";
+}
+
+void TestQString::toUpper()
+{
+    QFETCH(QString, string);
+    QFETCH(QString, result);
+
+    QCOMPARE(string.toUpper(), result);
+}
+
+QTEST_APPLESS_MAIN(TestQString)
+
+#include "tst_testqstring.moc"
