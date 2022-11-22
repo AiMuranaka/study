902f5117566da8981d14ab52d83a58bf3e8bd25c
AiMuranaka <ai.muranaka@carrot.co.jp>
2022-11-22 17:27:35 +0900

    implement TetrixTest2

--- Qt/Tetrix/TetrixTest/tst_tetrixtest.cpp
+++ Qt/Tetrix/TetrixTest/tst_tetrixtest.cpp
@@ -7,125 +7,50 @@ class TetrixTest : public QObject
     Q_OBJECT
 
 private slots:
+    void minX_data();
     void test_minX();
-    void test_maxX();
-    void test_minY();
-    void test_maxY();
 };
+
+void TetrixTest::minX_data()
+{
+    QTest::addColumn<int>("expected");
+    QTest::addColumn<int>("result");
+
+    TetrixPiece test;
+    test.setShape(NoShape);
+    QTest::newRow("positive value") << test.minX() << 0;
+
+    test.setShape(ZShape);
+    QTest::newRow("ZShape") << test.minX() << -1;
+
+    test.setShape(SShape);
+    QTest::newRow("SShape") << test.minX() << 0;
+
+    test.setShape(LineShape);
+    QTest::newRow("LineShape") << test.minX() << 0;
+
+    test.setShape(TShape);
+    QTest::newRow("TShape") << test.minX() << -1;
+
+    test.setShape(SquareShape);
+    QTest::newRow("SquareShape") << test.minX() << 0;
+
+    test.setShape(LShape);
+    QTest::newRow("LShape") << test.minX() << -1;
+
+    test.setShape(MirroredLShape);
+    QTest::newRow("MirroredLShape") << test.minX() << 0;
+
+
+}
 
 void TetrixTest::test_minX()
 {
-    TetrixPiece piece;
-    piece.setShape(NoShape);
-    QCOMPARE(piece.minX(), 0);
+     QFETCH(int, expected);
+     QFETCH(int, result);
 
-    piece.setShape(ZShape);
-    QCOMPARE(piece.minX(), -1);
-
-    piece.setShape(SShape);
-    QCOMPARE(piece.minX(), 0);
-
-    piece.setShape(LineShape);
-    QCOMPARE(piece.minX(), 0);
-
-    piece.setShape(TShape);
-    QCOMPARE(piece.minX(), -1);
-
-    piece.setShape(SquareShape);
-    QCOMPARE(piece.minX(), 0);
-
-    piece.setShape(LShape);
-    QCOMPARE(piece.minX(), -1);
-
-    piece.setShape(MirroredLShape);
-    QCOMPARE(piece.minX(), 0);
+     QCOMPARE(expected, result);
 }
-
-void TetrixTest::test_maxX()
-{
-    TetrixPiece piece;
-    piece.setShape(NoShape);
-    QCOMPARE(piece.maxX(), 0);
-
-    piece.setShape(ZShape);
-    QCOMPARE(piece.maxX(), 0);
-
-    piece.setShape(SShape);
-    QCOMPARE(piece.maxX(), 1);
-
-    piece.setShape(LineShape);
-    QCOMPARE(piece.maxX(), 0);
-
-    piece.setShape(TShape);
-    QCOMPARE(piece.maxX(), 1);
-
-    piece.setShape(SquareShape);
-    QCOMPARE(piece.maxX(), 1);
-
-    piece.setShape(LShape);
-    QCOMPARE(piece.maxX(), 0);
-
-    piece.setShape(MirroredLShape);
-    QCOMPARE(piece.maxX(), 1);
-}
-
-void TetrixTest::test_minY()
-{
-    TetrixPiece piece;
-    piece.setShape(NoShape);
-    QCOMPARE(piece.minY(), 0);
-
-    piece.setShape(ZShape);
-    QCOMPARE(piece.minY(), -1);
-
-    piece.setShape(SShape);
-    QCOMPARE(piece.minY(), -1);
-
-    piece.setShape(LineShape);
-    QCOMPARE(piece.minY(), -1);
-
-    piece.setShape(TShape);
-    QCOMPARE(piece.minY(), 0);
-
-    piece.setShape(SquareShape);
-    QCOMPARE(piece.minY(), 0);
-
-    piece.setShape(LShape);
-    QCOMPARE(piece.minY(), -1);
-
-    piece.setShape(MirroredLShape);
-    QCOMPARE(piece.minY(), -1);
-}
-
-void TetrixTest::test_maxY()
-{
-    TetrixPiece piece;
-    piece.setShape(NoShape);
-    QCOMPARE(piece.maxY(), 0);
-
-    piece.setShape(ZShape);
-    QCOMPARE(piece.maxY(), 1);
-
-    piece.setShape(SShape);
-    QCOMPARE(piece.maxY(), 1);
-
-    piece.setShape(LineShape);
-    QCOMPARE(piece.maxY(), 2);
-
-    piece.setShape(TShape);
-    QCOMPARE(piece.maxY(), 1);
-
-    piece.setShape(SquareShape);
-    QCOMPARE(piece.maxY(), 1);
-
-    piece.setShape(LShape);
-    QCOMPARE(piece.maxY(), 1);
-
-    piece.setShape(MirroredLShape);
-    QCOMPARE(piece.maxY(), 1);
-}
-
-
 QTEST_MAIN(TetrixTest)
 
 #include "tst_tetrixtest.moc"
